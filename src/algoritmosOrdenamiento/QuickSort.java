98b3a02c0fd60cba13a11129613523620c81a656
florencia-cicarone <63126991+florencia-cicarone@users.noreply.github.com>
2020-06-12 19:24:46 -0300

    Add files via upload

--- src/algoritmosOrdenamiento/QuickSort.java
+++ src/algoritmosOrdenamiento/QuickSort.java
@@ -30,23 +30,22 @@ public class QuickSort<T extends Comparable<T>> extends Ordenamiento<T> {
 		T pivot = arreglo[superior];
 		int i = (inferior - 1);
 		for (int j = inferior; j < superior; j++) {
-			pasos.add(new Paso(TipoPaso.COMPARACION, 0, j, superior, superior,true));
-			//long tiempoIniComp = System.nanoTime();
+			long tiempoIniComp = System.nanoTime();			
 			if (arreglo[j].compareTo(pivot) < 0) {
-				//long tiempoIniInt = System.nanoTime();
 				i++;
+				long tiempoIniInt = System.nanoTime();
 				intercambiar(arreglo, i, j);
-				//long tiempoFinInt = System.nanoTime();
-				pasos.add(new Paso(TipoPaso.INTERCAMBIO, 0, i, j, superior,true));
+				long tiempoFinInt = System.nanoTime();
+				pasos.add(new Paso(TipoPaso.INTERCAMBIO, tiempoFinInt - tiempoIniInt, i, j, superior,true));
 			}
-			//long tiempoFinComp = System.nanoTime();
-			
+		long tiempoFinComp = System.nanoTime();
+		pasos.add(new Paso(TipoPaso.COMPARACION, tiempoFinComp - tiempoIniComp, j, superior, superior,true));	
 		}
-		//long tiempoIniInt = System.nanoTime();
+		long tiempoIniInt = System.nanoTime();
 		intercambiar(arreglo, i + 1, superior);
-		//long tiempoFinInt = System.nanoTime();
-		pasos.add(new Paso(TipoPaso.INTERCAMBIO, 0, i + 1, superior, superior,true));
+		long tiempoFinInt = System.nanoTime();
+		pasos.add(new Paso(TipoPaso.INTERCAMBIO, tiempoFinInt - tiempoIniInt , i + 1, superior, superior,true));
 		return i + 1;
 	}
-
+	
 }
