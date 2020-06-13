d6d4277ba3a5c5ec7cac6dfd758240df484584bf
Noeli Sasha <noese63@gmail.com>
2020-06-12 21:17:39 -0300

    cambios graficos

--- src/graficos/OrdenamientoGrafico.java
+++ src/graficos/OrdenamientoGrafico.java
@@ -80,7 +80,7 @@ public class OrdenamientoGrafico extends JFrame {
 		pack();
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		//setResizable(false);
+		setResizable(false);
 		setTitle("jOrdenamiento");
 		setVisible(true);
 		setFocusable(true);
@@ -108,16 +108,18 @@ public class OrdenamientoGrafico extends JFrame {
 			g2.drawString("Tiempo: " + String.format("%6s", tiempoTranscurrido+ " ns"), 20, 65);
 			g2.drawString("Algoritmo: " + setUp.getAlgoritmo(), 200, 25);
 			g2.drawString("Cant. Elementos: " + setUp.getCantElementos(), 200, 45);
-			g2.fillRect(390, 3, 190, 60);
+			g2.fillRect(390, 3, 298, 70);
 			g2.setColor(Color.WHITE);
 			g2.drawString("No ordenado", 400, 25);
 			g2.setColor(Color.PINK);
 			g2.drawString("Comparando", 400, 45);
-			g2.setColor(Color.GRAY);
-			g2.drawString("Ordenado", 500, 25);
-			
 			g2.setColor(Color.CYAN);
-			g2.drawString("Pivot", 500, 45);
+			g2.drawString("Pivot (QS)", 500, 25);
+			g2.setColor(Color.RED);
+			g2.drawString("Posible Intercambio (QS)", 500, 45);
+			
+			g2.setColor(Color.GRAY);
+			g2.drawString("Ordenado", 400, 65);
 			
 			
 			int i = 0;
